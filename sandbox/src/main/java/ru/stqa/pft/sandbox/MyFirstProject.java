80c7fcdbfdb0c45e4c1e575ebc664ef4a3ac9c22
Janeoldman1 <2413200@gmail.com>
2019-02-24 11:47:17 -0500

    project was disconnected from repositirium

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProject.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProject.java
@@ -2,7 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProject {
 
-   public static void main(String[] args) {
+  public static void main(String[] args) {
 
-      System.out.println("Hello, world!"); }
+    System.out.println("Hello, world!");
+  }
 }

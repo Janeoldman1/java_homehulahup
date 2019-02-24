d7d0b10906980094bca674862deb8bdc02d11ddd
Janeoldman1 <2413200@gmail.com>
2019-02-24 11:13:12 -0500

    Pocket for my first program was created

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProject.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProject.java
@@ -1,8 +1,8 @@
+package ru.stqa.pft.sandbox;
+
 public class MyFirstProject {
 
    public static void main(String[] args) {
 
-      System.out.println("Hello, world!");
-
- }
+      System.out.println("Hello, world!"); }
 }

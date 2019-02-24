23d7deb99341faeaa67ebc9099634defc70d88b0
Janeoldman1 <2413200@gmail.com>
2019-02-24 16:07:18 -0500

    some features was added

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProject.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProject.java
@@ -3,7 +3,25 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProject {
 
   public static void main(String[] args) {
+    hello ("world");
+    hello ("user");
+    hello ("Eve");
 
-    System.out.println("Hello, world!");
+    double l = 5;
+    System.out.println("area square with side " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("area rectangle with side " + a + "  and " + b + " = " + area(a, b));
+    }
+
+  public static void hello (String somebody) {
+  System.out.println("Hello, "+somebody+"!");
+}
+public static double area (double len) {
+    return 1*1;
+}
+  public static double area (double a, double b){
+    return a*b;
   }
 }
